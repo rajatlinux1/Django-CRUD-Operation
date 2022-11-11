8e2a4d86322d68d044867341868eeaef4ada082a
Rajat Saini <103591089+rajatlinux1@users.noreply.github.com>
2022-11-12 00:11:22 +0530

    Delete forms.py

--- MyApp/forms.py
+++
@@ -1,7 +0,0 @@
-# from attr import field
-# from django import forms
-# from .models import student
-# class studentform(forms.ModelForm):
-#     class Meta():
-#         model=student
-#         fields=['name', 'age', 'email']
